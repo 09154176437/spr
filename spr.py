3298f3b21d8177aa1e313fbc31603e39fe80d843
09154176437 <88371981+09154176437@users.noreply.github.com>
2021-08-19 16:41:01 +0430

    Add files via upload

---
+++ spr.py
@@ -0,0 +1,18 @@
+# import random
+# items =["sang","kaghaz","ghychi"]
+# computer_coice = random.choice(items)
+# print("1-sang")
+# print("2-kaghaz")
+# print("3-ghychi")
+
+# user_choice_index = int(input())
+# user_choice = items[user_choice_index]
+
+# if computer_choice =="sang" and user_choice == "ghychi" :
+#     print("computer wins")
+# elif computer_choice == "kaghaz" and user_choice == "sang" :
+#     print("computer wins")
+# elif computer_choice == "ghychi" and user_choice == "kaghaz" :
+#     print("computer wins")
+# else :
+#         print("eeror")
